67fe1f450b7baee7422d9081e43bc8a889c2b21e
karnamin <aminkarn18@gmail.com>
2019-04-14 16:43:56 +0100

    initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Titles = props => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find out temperature, conditions amd more...</p>
+    </div>
+);
+
+export default Titles;
